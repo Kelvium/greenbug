e6599a46ed4b67019075342a6d7ab1fc5a4224e1
Kelvium <ufh8945@gmail.com>
2021-02-11 22:57:55 +0300

    LibC: implement

---
+++ Libraries/LibC/LibC.cmake
@@ -0,0 +1,3 @@
+list(APPEND LIBC_SRC
+    stdint.h
+)
---
+++ Libraries/LibC/stdint.h
@@ -0,0 +1,15 @@
+//
+// Created by kelvium on 11.02.2021.
+//
+
+#pragma once
+
+// Fixed-size types
+typedef signed char i8;
+typedef unsigned char u8;
+typedef signed short int i16;
+typedef unsigned short int u16;
+typedef signed int i32;
+typedef unsigned int u32;
+typedef signed long int i64;
+typedef unsigned long int u64;
